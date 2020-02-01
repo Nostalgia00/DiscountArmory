feb5753837fc3f0d6fee7168e1d5d0b9311ec165
bcarroll <bcarroll@guidewire.com>
2020-02-01 14:00:33 +0000

    Now detects collision on proper edge

--- Assets/Scripts/Controllers/StaticItemController.cs
+++ Assets/Scripts/Controllers/StaticItemController.cs
@@ -3,7 +3,26 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StaticItemController : MonoBehaviour {
+
+    public GameObject draggableItem;
+    private Collider2D draggableCollider;
+    float distance;
+    void Start() {
+        draggableCollider = draggableItem.GetComponents<Collider2D>()[1];
+    }
+
+    void Update() {
+        distance = GetComponents<Collider2D>()[1].Distance(draggableCollider).distance;
+        if(distance >= 0.01f) {
+            Debug.Log("Red");
+        } else if(distance < 0.01f && distance >=-0.0005f) {
+            Debug.Log("Amber");
+        } else {
+            Debug.Log("Green");
+        }
+
+        Debug.Log("Distance= " + distance);
+    }
     void OnCollisionEnter2D (Collision2D coll) {
-        Debug.Log("AAAAAA");
     }
 }
