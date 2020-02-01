7f8355da435610e8def9c4a5b596d2280efbf96c
Nukk Pointer <nukkpointer@gmail.com>
2020-02-01 12:55:22 +0000

    Game score text logic

--- Assets/scripts/controllers/GameController.cs
+++ Assets/scripts/controllers/GameController.cs
@@ -13,7 +13,7 @@ using UnityEngine.SceneManagement;
 public class GameController : MonoBehaviour
 {
 
-    static long overallGameScore = 0L;
+    public static long overallGameScore = 0L;
     // Start is called before the first frame update
     void Start()
     {
