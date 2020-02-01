a277cbfa01e4a384853fe83ccfe41b7f9acd8146
bcarroll <bcarroll@guidewire.com>
2020-02-01 10:49:36 +0000

    Adding 2D sprite collision detection

---
+++ Assets/Scripts/Controllers/FixableItemController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixableItemController : MonoBehaviour {
+    private Vector2 initialPos;
+
+    private Vector2 mousePosition;
+
+    private float deltaX, deltaY;
+
+    public static bool locked = false;
+
+    // Init
+    void Start () {
+        initialPos = transform.position;
+    }
+
+    private void OnMouseDown () {
+        if (!locked) {
+            deltaX = Camera.main.ScreenToWorldPoint (Input.mousePosition).x - transform.position.x;
+            deltaY = Camera.main.ScreenToWorldPoint (Input.mousePosition).y - transform.position.y;
+        }
+    }
+
+    private void OnMouseDrag () {
+        if (!locked) {
+            mousePosition = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+            transform.position = new Vector2 (mousePosition.x - deltaX, mousePosition.y - deltaY);
+        }
+    }
+
+    private void OnMouseUp () {
+
+    }
+}
---
+++ Assets/Scripts/Controllers/StaticItemController.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticItemController : MonoBehaviour {
+    void OnCollisionEnter2D (Collision2D coll) {
+        Debug.Log("AAAAAA");
+    }
+}
