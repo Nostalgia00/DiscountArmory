55533dc7d3586f7512beb0632e7d8dfe59e11650
bcarroll <bcarroll@guidewire.com>
2020-02-02 11:43:53 +0000

    Adding hammergame

---
+++ Assets/Scripts_1/Controllers/DragonController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; // Required when Using UI elements.
+
+public class DragonController : MonoBehaviour {
+
+    public float increment, holdTime, currentHoldTime;
+
+    void Start () {
+        currentHoldTime = holdTime;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        Image flameFill = GetComponent<Image> ();
+        if(Input.GetKeyDown(KeyCode.Return) ) {
+            flameFill.fillAmount -= 0.15f;
+
+        }
+        if (Input.GetKeyDown (KeyCode.Space) && flameFill.fillAmount <= 1.0f) {
+            flameFill.fillAmount += increment;
+            if (flameFill.fillAmount <= 0.5f) {
+                currentHoldTime = holdTime;
+
+            }
+
+        } else if (flameFill.fillAmount >= 0.95f && currentHoldTime > 0f) {
+            currentHoldTime -= Time.deltaTime;
+            if (currentHoldTime < 0f) {
+                currentHoldTime = 0f;
+            }
+        } else if (flameFill.fillAmount >= 0.0f) {
+            float decayRate = 0.05f;
+            flameFill.fillAmount -= decayRate * Time.deltaTime;
+
+        }
+    }
+}
---
+++ Assets/Scripts_1/Controllers/PowerBarController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PowerBarController : MonoBehaviour {
+    static float t = 0.0f;
+    public GameObject dragonObj;
+    public float sweetSpotMin, sweetSpotMax;
+    private Image dragonImage;
+
+    private bool locked;
+
+    private Slider powerBar;
+    // Start is called before the first frame update
+    void Start () {
+        locked = true;
+        powerBar = GetComponent<Slider> ();
+        dragonImage = dragonObj.GetComponent<Image>();
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKeyDown (KeyCode.Return) && locked && dragonImage.fillAmount >= 0.75f) {
+            locked = !locked;
+        }
+        else if(dragonImage.fillAmount <=0f && !locked) {
+            t=0;
+            powerBar.value=0;
+            locked = !locked;
+        }
+        else if (Input.GetKeyDown (KeyCode.Return) && dragonImage.fillAmount > 0f) {
+            if(powerBar.value >=sweetSpotMin && powerBar.value <= sweetSpotMax) {
+                Debug.Log("BING - GOOD");
+            } else {
+                Debug.Log("BONG - BAD");
+            }
+        }
+        
+        if (!locked) {
+            t += Time.deltaTime;
+            powerBar.value = Mathf.Abs(Mathf.Sin (t));
+        }
+
+    }
+}
