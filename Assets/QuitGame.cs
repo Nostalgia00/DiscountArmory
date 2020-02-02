2b0492a3cafb51f0c65b1fc9b17eb83725aa9f05
Nukk Pointer <nukkpointer@gmail.com>
2020-02-02 12:29:57 +0000

    Button messing

---
+++ Assets/QuitGame.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class QuitGame : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    public void ExitGame()
+    {
+        Debug.LogError("Button pressed!");
+        Application.Quit();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
