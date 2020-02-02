48532fd9cd94b76a40115c636eda04dbe25ef75f
bcarroll <bcarroll@guidewire.com>
2020-02-02 15:25:16 +0000

    Adding basic UI navigation

---
+++ Assets/ForgeButtonCanvasController.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class ForgeButtonCanvasController : MonoBehaviour
+{
+    public void HandleOnClick() {
+        SceneManager.LoadScene ("SwordScene", LoadSceneMode.Single);
+
+    }
+
+}
---
+++ Assets/Scripts_1/Controllers/CanvasController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasController : MonoBehaviour {
+    public float aValue = 1;
+    private CanvasGroup trans;
+    void Start () {
+        trans = GetComponent<CanvasGroup> ();
+        trans.alpha = aValue;
+    }
+}
